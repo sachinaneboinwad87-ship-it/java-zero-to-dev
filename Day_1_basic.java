a1a5d417cd428f87b974b440c92a756696715514
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2026-03-24 19:03:30 +0530

    Day 1: Java basics programs

---
+++ Day_1_basic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day_1_basic {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
